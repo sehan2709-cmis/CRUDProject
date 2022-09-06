9bf5e799b3ff618f87be99467b79b733bf2180ef
Sehan Kwon <sehan2020@gmail.com>
2022-09-07 02:18:46 +0900

    added some methods and codes for each classes

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -1,4 +1,49 @@
 package org.example;
 
 public class Word {
+    private int id;
+    private int level;
+    private String word;
+    private String meaning;
+
+    public Word() {}
+
+    public Word(int id, int level, String word, String meaning) {
+        this.id = id;
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
 }
--- src/main/java/org/example/WordMan.java
+++ src/main/java/org/example/WordMan.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class WordMan {
+    public void start(){
+        System.out.println("Word Manager!");
+    }
+
 }
